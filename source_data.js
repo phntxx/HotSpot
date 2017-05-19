09a68a817177c2b6a5c352d4d77a2669849389eb
phntxx <meissner.bastian@gmail.com>
2017-05-19 22:45:23 +0200

    i forgot

--- source_data.js
+++ source_data.js
@@ -65,25 +65,7 @@ eqfeed_callback({
       },
       {
          "type":"Feature",
-         "properties":{
-            "mag":3.8,
-            "place":"43km NNE of Talkeetna, Alaska",
-            "time":1348174056,
-            "tz":-480,
-            "url":"http://earthquake.usgs.gov/earthquakes/eventpage/ak10562838",
-            "felt":0,
-            "cdi":1,
-            "mmi":null,
-            "alert":null,
-            "status":"REVIEWED",
-            "tsunami":"1",
-            "sig":"222",
-            "net":"ak",
-            "code":"10562838",
-            "ids":",ak10562838,at00mao1rc,",
-            "sources":",ak,at,",
-            "types":",dyfi,general-link,geoserve,impact-link,nearby-cities,origin,tectonic-summary,"
-         },
+         "properties":{},
          "geometry":{
             "type":"Point",
             "coordinates":[
